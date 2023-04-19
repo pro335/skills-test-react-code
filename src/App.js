22efce7d38e1089226e7c826d3dcab93223ec472
pro335 <pawlbai124@gmail.com>
2023-04-19 11:22:39 -0500

    fixed errors that prevent the project from being built and run

--- src/App.js
+++ src/App.js
@@ -10,21 +10,21 @@ export default function App() {
 	const [openForm, setOpenForm] = React.useState(false);
 	const handleOnClick = (page) => {
 		console.log('open');
-		if(!openForm) { 
+		if (!openForm) {
 			setOpenForm(true)
 		} else {
 			setOpenForm(false)
 		}
 	};
-  return (
-    <Container maxWidth="sm">
-	<AccountMenu onClick={handleOnClick} />
-	{openForm && <Basicorm />}
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          React Skills test
-        </Typography>
-      </Box>
-    </Container>
-  );
+	return (
+		<Container maxWidth="sm">
+			<AccountMenu onClick={handleOnClick} />
+			{openForm && <BasicForm />}
+			<Box sx={{ my: 4 }}>
+				<Typography variant="h4" component="h1" gutterBottom>
+					React Skills test
+				</Typography>
+			</Box>
+		</Container>
+	);
 }
